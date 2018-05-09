960360072c463d0ac3f2264cc49a629540998e34
fujimuradaiki <39039677+fujimuradaiki@users.noreply.github.com>
2018-05-09 11:01:43 +0900

    topページ用の各種ファイルを追加

---
+++ Api/connectdb.php
@@ -0,0 +1,22 @@
+<?php
+
+class connectDB{
+    //データーベース接続のための変数宣言
+    private $DBuser ='root';
+    private $DBpass ='tgoda100100';
+    private $DBhost ='192.168.63.128';
+    private $DBname ='DWISystemdb';
+
+    public  $dbo;
+
+    public function __construct(){
+        try{
+            //データベースに接続
+            $this->dbo = new PDO('mysql:host=192.168.63.128; dbname=clientdb; charset=utf8mb4',$this->DBuser,$this->DBpass);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+            exit;
+        };
+    }
+}
+?>
