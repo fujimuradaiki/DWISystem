bc9555ed1a7fc13dbef0f10ac667463f0bd8efb2
imaizumi <imaizumi01281@gmail.com>
2018-05-10 15:15:54 +0900

    DBに接続できるように修正

--- Api/connectdb.php
+++ Api/connectdb.php
@@ -1,10 +1,10 @@
 <?php
 
-class connectDB{
+class connectdb{
     //データーベース接続のための変数宣言
     private $DBuser ='root';
     private $DBpass ='tgoda100100';
-    private $DBhost ='192.168.63.128';
+    private $DBhost ='localhost';
     private $DBname ='DWISystem_testdb';
 
     public  $dbo;
@@ -12,12 +12,11 @@ class connectDB{
     public function __construct(){
         try{
             //データベースに接続
-            $this->dbo = new PDO('mysql:host=192.168.63.128; dbname=DWISystem_testdb; charset=utf8mb4',$this->DBuser,$this->DBpass);
+            $this->dbo = new PDO('mysql:host=localhost; dbname=DWISystem_testdb; charset=utf8mb4',$this->DBuser,$this->DBpass);
+
         }catch(PDOException $e){
             echo $e->getMessage();
-           // echo "接続OK";
             exit;
         };
     }
 }
-?>
