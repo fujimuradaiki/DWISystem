2a8e1e14f5966167bce4e2c7d00956a66e6ea9ea
imaizumi <imaizumi01281@gmail.com>
2018-05-14 11:19:20 +0900

    コンフリクト後の修正版　トップ画面　一覧表示データ ソート・フィルタ機能なし

--- Api/connectdb.php
+++ Api/connectdb.php
@@ -1,18 +1,18 @@
 <?php
 
-class connectDB{
+class connectdb{
     //データーベース接続のための変数宣言
     private $DBuser ='root';
     private $DBpass ='tgoda100100';
     private $DBhost ='192.168.63.128';
-    private $DBname ='DWISystemdb';
+    private $DBname ='DWISystem_testdb';
 
     public  $dbo;
 
     public function __construct(){
         try{
             //データベースに接続
-            $this->dbo = new PDO('mysql:host=192.168.63.128; dbname=clientdb; charset=utf8mb4',$this->DBuser,$this->DBpass);
+            $this->dbo = new PDO('mysql:host=localhost; dbname=DWISystem_testdb; charset=utf8mb4',$this->DBuser,$this->DBpass);
         }catch(PDOException $e){
             echo $e->getMessage();
             exit;
