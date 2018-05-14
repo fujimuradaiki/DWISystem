52b01de20a6d2c5dc1e3808235bb5ada0e650e9e
fujimuradaiki <39039677+fujimuradaiki@users.noreply.github.com>
2018-05-14 11:27:11 +0900

    コンフリクト解消

--- Api/connectdb.php
+++ Api/connectdb.php
@@ -5,22 +5,15 @@ class connectdb{
     private $DBuser ='root';
     private $DBpass ='tgoda100100';
     private $DBhost ='192.168.63.128';
-<<<<<<< HEAD
-    private $DBname ='DWISystem_TESTdb';
-=======
     private $DBname ='DWISystem_testdb';
->>>>>>> origin/imaizumi
+
 
     public  $dbo;
 
     public function __construct(){
         try{
             //データベースに接続
-<<<<<<< HEAD
-            $this->dbo = new PDO('mysql:host=192.168.63.128; dbname=DWISystem_TESTdb; charset=utf8mb4',$this->DBuser,$this->DBpass);
-=======
             $this->dbo = new PDO('mysql:host=localhost; dbname=DWISystem_testdb; charset=utf8mb4',$this->DBuser,$this->DBpass);
->>>>>>> origin/imaizumi
         }catch(PDOException $e){
             echo $e->getMessage();
             exit;
--- Api/controller.php
+++ Api/controller.php
@@ -7,17 +7,9 @@ header('Content-type: application/json');
 if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])
     && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
 
-<<<<<<< HEAD
-   //     $test_model = "user";
-     //   $test_action = "test";
-      //$classname = new $_POST['model'];
-      //$classname->controller($_POST['action']);
-        $classname = new $test_model;
-        $classname->controller($test_action);
-=======
+
         $classname = new $_POST['model'];
         $classname->controller($_POST['action'],$_POST['data']);
->>>>>>> origin/imaizumi
 }else{
     echo "else!";
 
