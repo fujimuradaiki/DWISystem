10b890c1067b2acb262c4be2936ba3af2124cc1f
murai-suzuka <murai-suzuka@social-sharing.org>
2018-05-16 16:43:06 +0900

    ログイン画面、新規登録画面、新規登録内容確認のポップ作成

--- View/JS/lightbox.js
+++ View/JS/lightbox.js
@@ -10,6 +10,26 @@ $('.login_btn2').click(function(){
   $('body').addClass("overflow");
 });
 
+$('.new_btn').click(function(){
+  $('.new_view').fadeIn();
+  $('body').addClass("overflow");
+});
+
+$('.confirmation_btn').click(function(){
+  $('.new_confirmation_view').fadeIn();
+  $('body').addClass("overflow");
+});
+
+$('.return_btn').click(function(){
+  $('.new_view').fadeIn();
+  $('body').addClass("overflow");
+});
+
+$('.touroku_btn').click(function(){
+  $('.new_touroku_view').fadeIn();
+  $('body').addClass("overflow");
+});
+
   $('.images #1').click(function(){
   $('.lightbox_view1').fadeIn();
   $('body').addClass("overflow");
@@ -46,12 +66,36 @@ $('#6.images').click(function(){
 // close�{�^��//
 
 $('.close_btn img').click(function(){//.close_btn img���N���b�N�����Ƃ�//
-  $('.lightbox_view1,.lightbox_view2,.lightbox_view3,.lightbox_view4,.lightbox_view5,.lightbox_view6,.login_view').fadeOut();//view���t�F�[�h�A�E�g����//
+  $('.lightbox_view1,.lightbox_view2,.lightbox_view3,.lightbox_view4,.lightbox_view5,.lightbox_view6,.login_view,.new_view').fadeOut();//view���t�F�[�h�A�E�g����//
+  $('body').removeClass("overflow");
+});
+
+$('.login_close_btn img').click(function(){//.close_btn img���N���b�N�����Ƃ�//
+  $('.login_view').fadeOut();//view���t�F�[�h�A�E�g����//
+  $('body').removeClass("overflow");
+});
+
+$('.new_close_btn img').click(function(){//.close_btn img���N���b�N�����Ƃ�//
+  $('.new_view,.new_confirmation_view,.new_touroku_view').fadeOut();//view���t�F�[�h�A�E�g����//
   $('body').removeClass("overflow");
 });
 
 $('.close_btn2').click(function(){//.close_btn img���N���b�N�����Ƃ�//
-  $('.login_view').fadeOut();//view���t�F�[�h�A�E�g����//
+  $('.login_view,.new_view,.new_touroku_view').fadeOut();//view���t�F�[�h�A�E�g����//
+  $('body').removeClass("overflow");
+});
+
+$('.confirmation_btn').click(function(){//.close_btn img���N���b�N�����Ƃ�//
+  $('.new_view').fadeOut();//view���t�F�[�h�A�E�g����//
+  $('body').removeClass("overflow");
+});
+$('.return_btn').click(function(){//.close_btn img���N���b�N�����Ƃ�//
+  $('.new_confirmation_view').fadeOut();//view���t�F�[�h�A�E�g����//
+  $('body').removeClass("overflow");
+});
+
+$('.touroku_btn').click(function(){//.close_btn img���N���b�N�����Ƃ�//
+  $('.new_confirmation_view').fadeOut();//view���t�F�[�h�A�E�g����//
   $('body').removeClass("overflow");
 });
 
