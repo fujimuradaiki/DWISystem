627603a3a95c3c6388e1c94c04205c3ae00404b7
imaizumi <imaizumi01281@gmail.com>
2018-05-29 17:22:22 +0900

    画像削除処理の追加

--- View/JS/deleteTest.js
+++ View/JS/deleteTest.js
@@ -42,5 +42,38 @@ $(document).on("click",".Completion_btn",function(){
 	else{
 		window.alert('キャンセルされました'); // 警告ダイアログを表示
 	}
+});
 
+$(document).on("click",".delete_btn",function(){
+	var imageId = $(".view_image").attr("id");
+	var userName = sessionStorage.getItem('userName');
+	var userId = sessionStorage.getItem('userId');
+	//デバック用
+	userName = "TestUser";
+
+	console.log(imageId,userName,userId);
+
+	var param = [];
+	param[0] = imageId;
+	param[1] = userName;
+	param[2] = userId;
+	var data = {'model':'images','action':'delete','data':param};
+	console.log(data);
+
+//	//ajax通信
+	$.ajax({
+		url:"../../Api/controller.php",
+		dataType:'json',
+		type:"POST",
+		data:data
+	//ajax通信成功時
+	}).done(function(data){
+		alert(data);
+		//runSearch();
+		$('.lightbox_view,.login_view,.new_view').fadeOut();
+		$('body').removeClass("overflow");
+	//ajax通信失敗時
+	}).fail(function(XMLHttpRequest, textStatus, errorThrown){
+		alert("error : " + textStatus);
+	});
 });
