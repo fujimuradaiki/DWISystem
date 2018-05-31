9df4f09decf2d55f60440aa7c04822126ef21a3c
murai-suzuka <murai-suzuka@social-sharing.org>
2018-05-31 09:36:36 +0900

    ログイン後アイコンクリック時のメニュー開閉動作追加

--- View/JS/dropdown.js
+++ View/JS/dropdown.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
- $('.login_user_icon').click(function(){
-  $('.login_user_menu').fadeIn();
- });
-
+$('.login_user_icon').click(function(){
+  $('.login_user_menu').fadeToggle();
 });
+
+
+ });
