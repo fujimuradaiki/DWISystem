f8397609841840a72ae9b7bfa0da104695d15d74
imaizumi <imaizumi01281@gmail.com>
2018-06-11 15:27:23 +0900

    接続変更

--- Api/connectdb.php
+++ Api/connectdb.php
@@ -2,15 +2,20 @@
 $pdo  = new connectdb();
 class connectdb{
     //データーベース接続のための変数宣言
-    private $DBuser ='root';
-    private $DBpass ='tgoda100100';
-    private $DBhost ='192.168.63.128';
-    private $DBname ='DWISystem_testdb';
+//     private $DBuser ='root';
+//     private $DBpass ='tgoda100100';
+//     private $DBhost ='192.168.63.128';
+//     private $DBname ='DWISystem_testdb';
 
 //     private $DBuser ='server0531_user';
 //     private $DBpass ='tgoda100100';
 //     private $DBhost ='mysql1.php.xdomain.ne.jp';
 //     private $DBname ='server0531_dwisystem';
+    private $DBuser ='root';
+    private $DBpass ='Yq8jNrvJ';
+    private $DBhost ='192.168.63.130';
+    private $DBname ='DWISystem_testdb';
+
 
     public  $dbo;
 
