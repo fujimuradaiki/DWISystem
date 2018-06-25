2d38b1d43f46653731f86144710ac820277bf1d5
WD-1722-01\inoue-saki <inoue-saki@social-sharing.org>
2018-06-25 10:16:38 +0900

    スライダーsp用画像トリミング、挿入

---
+++ View/JS/backslider.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  
+  if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+    
+    // PC slider
+    $('.parallax-bg').bgSwitcher({
+      images: [
+      '../Images/background2.png',
+      '../Images/background3.jpg',
+      '../Images/02.png',
+      '../Images/05.png',
+      '../Images/06.png',
+      '../Images/051.png'
+      ]
+    });
+  } else {
+    // SP slider
+    $('.parallax-bg').bgSwitcher({
+      images: [
+      '../Images/sp0512.png',
+      '../Images/sp04.png',
+      '../Images/sp03.png',
+      '../Images/sp02.png',
+      '../Images/sp05.png',
+      '../Images/sp06.png',
+      '../Images/sp051.png'
+      ]
+    });
+    
+//    $(this).prev("div").css("background-size","200px 200px"); 
+  
+  }
+});
--- View/JS/style.js
+++ View/JS/style.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-  $(".parallax-bg").bgswitcher({
-    images: [
-      // "../Images/background1.jpg",
-      "../Images/background2.png",
-      "../Images/background3.jpg",
-      "../Images/02.png",
-      "../Images/05.png",
-      "../Images/06.png",
-      "../Images/051.png",
-
-    ]
-  });
-});
+//$(document).ready(function(){
+//  $(".parallax-bg").bgswitcher({
+//    images: [
+//      // "../Images/background1.jpg",
+//      "../Images/background2.png",
+//      "../Images/background3.jpg",
+//      "../Images/02.png",
+//      "../Images/05.png",
+//      "../Images/06.png",
+//      "../Images/051.png",
+//
+//    ]
+//  });
+//});
