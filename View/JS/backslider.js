e6d49cca1e46b0352c02fa689be3d48a9ba2016a
WD-1722-01\inoue-saki <inoue-saki@social-sharing.org>
2018-06-27 17:19:53 +0900

    詳細ページ

--- View/JS/backslider.js
+++ View/JS/backslider.js
@@ -1,38 +1,18 @@
 $(document).ready(function(){
-  
-  if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android&Mobile)/)){
-
-    // PC slider
-   $('.parallax-bg').bgSwitcher({
-      images: [
-      '../Images/background2.png',
-      '../Images/background3.jpg',
-      '../Images/02.png',
-      '../Images/05.png',
-      '../Images/06.png',
-      '../Images/051.png'
-      ]
-    });
-       
-  } else {
-     // SP slider
-    $('.parallax-bg').bgSwitcher({
-      images: [
-      '../Images/sp0512.png',
-      '../Images/sp04.png',
-      '../Images/sp03.png',
-      '../Images/sp02.png',
-      '../Images/sp05.png',
-      '../Images/sp06.png',
-      '../Images/sp051.png'
-      ]
-    });
-    
-    alert($(".parallax-bg").prev("div").css("width"));
-    $(".parallax-bg").prev("div").css("width","100%");
-    
-    location.href = 'spindex.html';
-
-    
+  // リダイレクト
+  if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+    // sp
+    window.location.href = "../HTML/spindex.html";
   }
+
+  $('.parallax-bg').bgSwitcher({
+    images: [
+    '../Images/background2.png',
+    '../Images/background3.jpg',
+    '../Images/02.png',
+    '../Images/05.png',
+    '../Images/06.png',
+    '../Images/051.png'
+    ]
+  });
 });
